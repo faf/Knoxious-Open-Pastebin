34aafdfbb3ae83c97801bb37a2f8dbbec3c46f73
Fedor A. Fetisov <faf@ossg.ru>
2018-11-25 01:41:17 +0300

    Fix style in js

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
-var tab = "    ";
+var tab = '    ';
 
 function catchTab(evt) {
     var t = evt.target;
@@ -8,16 +8,16 @@ function catchTab(evt) {
     if (evt.keyCode == 9) {
         evt.preventDefault();
 
-        if (ss != se && t.value.slice(ss,se).indexOf("\n") != -1) {
-            var pre = t.value.slice(0,ss);
-            var sel = t.value.slice(ss,se).replace(/\n/g,"\n"+tab);
-            var post = t.value.slice(se,t.value.length);
+        if (ss != se && t.value.slice(ss, se).indexOf("\n") != -1) {
+            var pre = t.value.slice(0, ss);
+            var sel = t.value.slice(ss, se).replace(/\n/g, "\n" + tab);
+            var post = t.value.slice(se, t.value.length);
             t.value = pre.concat(tab).concat(sel).concat(post);
             t.selectionStart = ss + tab.length;
             t.selectionEnd = se + tab.length;
         }
         else {
-            t.value = t.value.slice(0,ss).concat(tab).concat(t.value.slice(ss,t.value.length));
+            t.value = t.value.slice(0, ss).concat(tab).concat(t.value.slice(ss, t.value.length));
             if (ss == se) {
                 t.selectionStart = t.selectionEnd = ss + tab.length;
             }
@@ -27,67 +27,67 @@ function catchTab(evt) {
             }
         }
     }
-    else if (evt.keyCode==8 && t.value.slice(ss - 4,ss) == tab) {
+    else if (evt.keyCode == 8 && t.value.slice(ss - 4, ss) == tab) {
         evt.preventDefault();
 
-        t.value = t.value.slice(0,ss - 4).concat(t.value.slice(ss,t.value.length));
+        t.value = t.value.slice(0, ss - 4).concat(t.value.slice(ss, t.value.length));
         t.selectionStart = t.selectionEnd = ss - tab.length;
     }
-    else if (evt.keyCode==46 && t.value.slice(se,se + 4) == tab) {
+    else if (evt.keyCode == 46 && t.value.slice(se, se + 4) == tab) {
         evt.preventDefault();
 
-        t.value = t.value.slice(0,ss).concat(t.value.slice(ss + 4,t.value.length));
+        t.value = t.value.slice(0, ss).concat(t.value.slice(ss + 4, t.value.length));
         t.selectionStart = t.selectionEnd = ss;
     }
-    else if (evt.keyCode == 37 && t.value.slice(ss - 4,ss) == tab) {
+    else if (evt.keyCode == 37 && t.value.slice(ss - 4, ss) == tab) {
         evt.preventDefault();
         t.selectionStart = t.selectionEnd = ss - 4;
     }
-    else if (evt.keyCode == 39 && t.value.slice(ss,ss + 4) == tab) {
+    else if (evt.keyCode == 39 && t.value.slice(ss, ss + 4) == tab) {
         evt.preventDefault();
         t.selectionStart = t.selectionEnd = ss + 4;
     }
 }
 
 function toggleAdminTools(hideMe) {
-    if (document.getElementById('hiddenAdmin').style.display == "block") {
-        document.getElementById('hiddenAdmin').style.display = "none";
+    if (document.getElementById('hiddenAdmin').style.display == 'block') {
+        document.getElementById('hiddenAdmin').style.display = 'none';
     }
     else {
-        document.getElementById('hiddenAdmin').style.display = "block";
+        document.getElementById('hiddenAdmin').style.display = 'block';
     }
     return false;
 }
 
 function toggleInstructions() {
     hideSubdomain();
-    if (document.getElementById('instructions').style.display == "block") {
-        document.getElementById('instructions').style.display = "none";
+    if (document.getElementById('instructions').style.display == 'block') {
+        document.getElementById('instructions').style.display = 'none';
     }
     else {
-        document.getElementById('instructions').style.display = "block";
+        document.getElementById('instructions').style.display = 'block';
     }
     return false;
 }
 
 function hideInstructions() {
-    document.getElementById('instructions').style.display = "none";
+    document.getElementById('instructions').style.display = 'none';
     return false;
 }
 
 function toggleSubdomain() {
     hideInstructions();
-    if (document.getElementById('subdomainForm').style.display == "block") {
-        document.getElementById('subdomainForm').style.display = "none";
+    if (document.getElementById('subdomainForm').style.display == 'block') {
+        document.getElementById('subdomainForm').style.display = 'none';
     }
     else {
-        document.getElementById('subdomainForm').style.display = "block";
+        document.getElementById('subdomainForm').style.display = 'block';
     }
     return false;
 }
 
 function hideSubdomain() {
-    document.getElementById('subdomainForm').style.display = "none";
+    document.getElementById('subdomainForm').style.display = 'none';
     return false;
 }
 
@@ -95,20 +95,20 @@ function toggleWrap() {
     var n = 0;
     var pres = document.getElementsByTagName('pre');
     for (n in pres) {
-        if (pres[n].style != null && (pres[n].style.whiteSpace == "pre" || pres[n].style.whiteSpace == "")) {
-            pres[n].style.whiteSpace = "pre-wrap";
+        if (pres[n].style != null && (pres[n].style.whiteSpace == 'pre' || pres[n].style.whiteSpace == '')) {
+            pres[n].style.whiteSpace = 'pre-wrap';
         }
         else if (pres[n].style != null) {
-            pres[n].style.whiteSpace = "pre";
+            pres[n].style.whiteSpace = 'pre';
         }
     }
     return false;
 }
 
 function toggleExpand() {
-    if (document.getElementById('lineNumbers').style.maxHeight != "none") {
-        document.getElementById('lineNumbers').style.maxHeight = "none";
-        document.getElementById('lineNumbers').style.width = "auto";
+    if (document.getElementById('lineNumbers').style.maxHeight != 'none') {
+        document.getElementById('lineNumbers').style.maxHeight = 'none';
+        document.getElementById('lineNumbers').style.width = 'auto';
     }
     else {
         document.getElementById('lineNumbers').setAttribute('style', '');
@@ -117,11 +117,11 @@ function toggleExpand() {
 }
 
 function toggleStyle(){
-    if (document.getElementById('orderedList').getAttribute('class') == "monoText" || document.getElementById('orderedList').getAttribute('class') == "") {
-        document.getElementById('orderedList').setAttribute("class", "plainText");
+    if (document.getElementById('orderedList').getAttribute('class') == 'monoText' || document.getElementById('orderedList').getAttribute('class') == '') {
+        document.getElementById('orderedList').setAttribute('class', 'plainText');
     }
     else {
-        document.getElementById('orderedList').setAttribute("class", "monoText");
+        document.getElementById('orderedList').setAttribute('class', 'monoText');
     }
     return false;
 }
@@ -133,7 +133,7 @@ function submitPaste(targetButton) {
     disabledButton.setAttribute('type', 'button');
     disabledButton.setAttribute('disabled', 'disabled');
     disabledButton.setAttribute('id', 'dummyButton');
-    targetButton.style.display = "none";
+    targetButton.style.display = 'none';
     parentContainer.appendChild(disabledButton);
     return true;
 }
