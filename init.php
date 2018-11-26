dc57e5c7a6840fbd74f7212b8e5725a56efe5b9d
Fedor A. Fetisov <faf@ossg.ru>
2018-11-26 17:35:40 +0300

    Implement PHP extensions check
    Fixes #7

--- init.php
+++ init.php
@@ -13,19 +13,35 @@ require_once('config.php');
 require_once('lib/classes/SPB/DB.php');
 require_once('lib/classes/SPB/Bin.php');
 
+// Callback function to handle magic quotes
+// (seriously, does anyone still use that?!)
 function callback_stripslashes(&$val, $name)
 {
     $val = stripslashes($val);
 }
 
+// Check required PHP version
 if (substr(phpversion(), 0, 3) < 5.3) {
-    die('PHP 5.3 is required to run this pastebin! This version is ' . phpversion() . '. Please contact your host!');
+    die('PHP 5.3 is required to run this pastebin. This version is ' . phpversion());
 }
 
+// Check required PHP extensions
+$extensions = array();
+if ($SPB_CONFIG['gzip_content']) {
+    $extensions[] = 'zlib';
+}
+foreach ($extensions as $ext) {
+    if (!extension_loaded($ext)) {
+        die('Missed required PHP ' . $ext . ' extension.');
+    }
+}
+
+// Initialize compression if needed
 if ($SPB_CONFIG['gzip_content']) {
     ob_start("ob_gzhandler");
 }
 
+// Handle cursed magic quotes
 if (get_magic_quotes_gpc()) {
     if (count($_GET)) {
         array_walk($_GET, 'callback_stripslashes');
