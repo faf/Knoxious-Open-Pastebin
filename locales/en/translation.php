6c511bc879a945f97265bcabbed42fbbd2dcb432
Fedor A. Fetisov <faf@ossg.ru>
2018-12-25 20:03:38 +0300

    Add function to translate relative times for English

--- locales/en/translation.php
+++ locales/en/translation.php
@@ -7,3 +7,10 @@ if (ISINCLUDED != '1') {
 // List of all localization constants
 // It's empty for 'en', but one is free to redefine own strings here
 $translation = array();
+
+// List of all language-specific functions
+$translation_functions = array(
+    'translate_time' => function($number, $units) {
+                            return $number . ' ' . ((int) $number === 1) ? substr($units, 0, -1) : $units;
+                        },
+);
